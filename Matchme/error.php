863ae72ea1148a21a75c605be1689c831603fe6f
MendezJennifer <77803373+MendezJennifer@users.noreply.github.com>
2021-04-04 12:00:12 -0600

    Added Log in and Register Pages, ability to store users in db

---
+++ Matchme/error.php
@@ -0,0 +1,12 @@
+<?php
+
+require('header.php'); 
+?>
+
+//Display erorr message and button that returns to index
+<h2> Oops! Something went wrong! </h2>
+<a href="index.php" class="btn btn-primary"> Home </a>
+
+<?php
+require('footer.php')
+?>
